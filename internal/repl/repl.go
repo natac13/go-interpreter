818e4af808425e8449b75bdc993134c230d2d405
Sean Campbell <sean.campbell13@gmail.com>
2024-04-29 12:31:59 -0400

    up to 3.6

--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/natac13/go-interpreter/internal/evaluator"
 	"github.com/natac13/go-interpreter/internal/lexer"
 	"github.com/natac13/go-interpreter/internal/parser"
 )
@@ -31,8 +32,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+
 	}
 }
 
